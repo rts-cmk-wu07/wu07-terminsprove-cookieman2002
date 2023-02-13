2179864cff0b3efe9bb16d1267e08ffa7423f517
cookieman2002 <mcl2002@outlook.dk>
2023-02-13 10:09:19 +0100

    start up

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      grey: "#E4E4E4",
+      orange: "#F4A88E"
+
+    },
+    fonts: {
+      
+    }, 
+    
+  },
+  plugins: [],
+}
